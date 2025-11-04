53995633c0ef5529aa3b5f1b482a45a9ee17cf80
krishaggarwal088 <krishagg088@gmail.com>
2025-11-04 21:15:21 +0530

    objects

---
+++ lesson 3/script1.js
@@ -0,0 +1,7 @@
+        var person = {
+            firstName: "I",
+                age: 20,
+
+            };
+            document.getElementById("demo" ). innerHTML = person.firstName
++ " am " + person.age + " years old";
