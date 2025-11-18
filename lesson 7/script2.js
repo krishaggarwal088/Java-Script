a6c4c8841ec2fc306f93df0a890112a2f376f558
krishaggarwal088 <krishagg088@gmail.com>
2025-11-18 21:33:55 +0530

    Data Type

---
+++ lesson 7/script2.js
@@ -0,0 +1,12 @@
+var a = 8*11;
+var b = 8*11;
+var c = 8;
+var d = 8+92-50+(-42);
+var e;
+document.write("a and b are equal : " + (a == b));
+document.write("<br>");
+document.write("b and c are equal : "+ (b == c));
+document.write("<br>");
+document.write("The value of d is : " + d);
+document.write("<br>");
+document.write("The value of e is : " + e)
